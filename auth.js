b19bafa0b1f22c4d3c71d3bf5fa9bcf6b1851922
alireh <ali.khezri1986@gmail.com>
2025-12-29 23:20:55 +0330

    changing auth

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-export const adminAuth = (req, res, next) => {
+export const adminAuth1 = (req, res, next) => {
   const header = req.headers.authorization;
   if (!header) return res.status(401).json({ error: 'No token' });
 
@@ -9,6 +9,22 @@ export const adminAuth = (req, res, next) => {
     req.admin = jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
+export const adminAuth = (req, res, next) => {
+  console.log('AUTH HEADER =>', req.headers.authorization);
+
+  const header = req.headers.authorization;
+  if (!header) return res.status(401).json({ error: 'No token' });
+
+  const token = header.split(' ')[1];
+
+  try {
+    req.admin = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    console.error('JWT VERIFY ERROR =>', err.message);
     res.status(401).json({ error: 'Invalid token' });
   }
 };
